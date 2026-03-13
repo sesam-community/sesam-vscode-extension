65f5b5f89a1897a1c03a254003ce381245ae5e4c
Shimi Zacken <shimi.zacken@sesam.io>
2026-03-13 14:19:10 +0100

    build: vite

---
+++ vite.config.client.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { builtinModules } from "module";
+
+export default defineConfig({
+  resolve: {
+    extensions: [".mts", ".ts", ".tsx", ".mjs", ".js", ".jsx", ".json"],
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "client/src/extension.ts"),
+      formats: ["cjs"],
+    },
+    outDir: "dist/client",
+    emptyOutDir: true,
+    sourcemap: true,
+    minify: false,
+    target: "node20",
+    rollupOptions: {
+      external: [
+        "vscode",
+        ...builtinModules,
+        ...builtinModules.map((m) => `node:${m}`),
+      ],
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+      },
+    },
+  },
+});
